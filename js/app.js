fadd6290551bd59a1fb5b767bf45215fcc0598b9
Noman Rafiq <heyimnomee@gmail.com>
2024-04-16 20:05:57 +0500

    before animation

--- js/app.js
+++ js/app.js
@@ -6,31 +6,12 @@ const date = (document.getElementById('date').innerHTML =
 // ********** nav toggle ************
 // select button and links
 const navBtn = document.getElementById('nav-toggle');
-const links = document.getElementById('nav-links');
+const links = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
 // add event listener
 navBtn.addEventListener('click', () => {
-    links.classList.toggle('show-links');
+    links.classList.add('show-links');
 });
-
-// ********** smooth scroll ************
-// select links
-const scrollLinks = document.querySelectorAll('.scroll-link');
-scrollLinks.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        // prevent default
-        e.preventDefault();
-        links.classList.remove('show-links');
-
-        const id = e.target.getAttribute('href').slice(1);
-        const element = document.getElementById(id);
-        //
-        let position = element.offsetTop - 62;
-
-        window.scrollTo({
-            left: 0,
-            // top: element.offsetTop,
-            top: position,
-            behavior: 'smooth',
-        });
-    });
+navClose.addEventListener('click', () => {
+    links.classList.remove('show-links');
 });
